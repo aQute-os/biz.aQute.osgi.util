710989e38ff1d013b507dff5d85fa9e973942263
Stefan Bischof <stbischof@bipolis.org>
2021-01-11 15:27:48 +0100

    use metatype Annotations for better doc
    Signed-off-by: Stefan Bischof <stbischof@bipolis.org>

--- biz.aQute.shell.sshd.provider/src/main/java/biz/aQute/shell/sshd/config/SshdConfigInsecure.java
+++ biz.aQute.shell.sshd.provider/src/main/java/biz/aQute/shell/sshd/config/SshdConfigInsecure.java
@@ -1,22 +1,25 @@
 package biz.aQute.shell.sshd.config;
 
+import org.osgi.service.metatype.annotations.AttributeDefinition;
 import org.osgi.service.metatype.annotations.ObjectClassDefinition;
 
 /**
  * Configuration for the Gogo SSH interface in an insecure mode.
  */
-@ObjectClassDefinition(factoryPid = SshdConfigInsecure.PID)
+@ObjectClassDefinition(factoryPid = SshdConfigInsecure.PID, description = "Configuration for the Gogo SSH interface in an insecure mode.")
 public @interface SshdConfigInsecure {
 	String PID = "biz.aQute.shell.sshd.insecure";
 
 	/**
 	 * The port to run on, by default this is {@value}
 	 */
+	@AttributeDefinition(description = "The port to run on")
 	int port() default 8061;
 
 	/**
 	 * Where to store the private key. This must be in forward slashes. Default is
-	 * 'host.ser' in the bundle's directory
+	 * 'target/host.ser' in the bundle's directory
 	 */
+	@AttributeDefinition(description = "Where to store the private key. This must be in forward slashes.")
 	String hostkey() default "target/host.ser";
 }
