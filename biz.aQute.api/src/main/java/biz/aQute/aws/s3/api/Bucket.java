c5d2bf8a9b76e9e7f4d24a28f913c8585ac22e35
Peter Kriens <Peter.Kriens@aqute.biz>
2021-03-24 11:14:18 +0100

    docs

--- biz.aQute.api/src/main/java/biz/aQute/aws/s3/api/Bucket.java
+++ biz.aQute.api/src/main/java/biz/aQute/aws/s3/api/Bucket.java
@@ -5,10 +5,56 @@ import java.net.URI;
 import java.util.Collection;
 import java.util.Date;
 
+/**
+ * A Bucket is a set of Objects. An Object is a blob with some meta data,
+ * including a path.
+ *
+ */
 public interface Bucket {
 
+	/**
+	 * Get the name of the bucket.
+	 *
+	 * @return the name of the bucket
+	 */
 	String getName();
-	public static class Content {
+
+	/**
+	 * Create a PUT request. The PUT request can be used to store content in the
+	 * object.
+	 *
+	 * @param key
+	 *            the path of the object
+	 * @return a request
+	 */
+	PutRequest putObject(String key);
+
+	/**
+	 * Create a GET request. The GET request can be used to get content from the
+	 * object.
+	 *
+	 * @param key
+	 *            the path of the object
+	 * @return a request
+	 */
+	GetRequest getObject(String key);
+
+	/**
+	 * Delete an Object
+	 *
+	 * @param key
+	 *            the path of the object
+	 */
+	void delete(String key) throws Exception;
+
+	/**
+	 * Create a LIST request
+	 *
+	 * @return a List Request
+	 */
+	ListRequest listObjects() throws Exception;
+
+	public class Content {
 		public Bucket		bucket;
 		public String		key;
 		public Date			lastModified;
@@ -17,7 +63,7 @@ public interface Bucket {
 		public String		etag;
 	}
 
-	public static class Range {
+	public class Range {
 		final public long	start;
 		final public long	length;
 
@@ -33,50 +79,160 @@ public interface Bucket {
 		}
 	}
 
+	/**
+	 * The Common part of all requests *
+	 *
+	 * @param <T>
+	 *            the actual request type
+	 */
 	public interface CommonRequest<T> {
-		T contentType(String string);
 
+		/**
+		 * Specify the content type
+		 *
+		 * @param contentType
+		 *            th
+		 */
+		T contentType(String contentType);
+
+		/**
+		 * ??
+		 */
 		T date(Date date);
 
+		/**
+		 * Specify the MD5 checksum that is the ETag on Amazon
+		 *
+		 * @param etag
+		 *            the etag
+		 */
 		T contentMD5(String etag);
 
-		T header(String header, String value);
+		/**
+		 * Specify a header
+		 *
+		 * @param name
+		 *            the name of the argument
+		 * @param value
+		 *            of the argument
+		 */
+		T header(String name, String value);
 
+		/**
+		 * Specify an argument
+		 *
+		 * @param name
+		 *            the name of the argument
+		 * @param value
+		 *            of the argument
+		 */
 		T argument(String name, String value);
 	}
 
+	/**
+	 * A request builder for a GET request.
+	 */
 	public interface GetRequest extends CommonRequest<GetRequest> {
+		/**
+		 * Specify a range to read
+		 *
+		 * @param range
+		 *            the range to read
+		 */
 		GetRequest range(Range range);
 
+		/**
+		 * Specify a set of ranges to read
+		 *
+		 * @param ranges
+		 *            the ranges to read
+		 */
 		GetRequest ranges(Collection<Range> range);
 
+		/**
+		 * The IF-MODIFIED header. Specifies that if the target object is
+		 * modified after the date it should be read.
+		 *
+		 * @param date
+		 *            the threshold date
+		 */
 		GetRequest ifModfiedSince(Date date);
 
+		/**
+		 * The IF-UNMODIFIED header. Specifies that if the target object is
+		 * not modified after the date it should be read.
+		 *
+		 * @param date
+		 *            the threshold date
+		 */
 		GetRequest ifUnmodfiedSince(Date date);
 
+		/**
+		 * The IF-MATCH header. Specifies that if the target object
+		 * has the same Etag
+		 *
+		 * @param etag the ETag
+		 */
 		GetRequest ifMatch(String etag);
 
+		/**
+		 * ??
+		 */
 		GetRequest ifNoneMatch(String etag);
 
+		/**
+		 * Set the content type
+		 */
 		GetRequest contentType(String string);
 
+		/**
+		 * Open the connection and get the bytes
+		 */
 		InputStream get() throws Exception;
 
+		/**
+		 * Create a signed URI that will expire after expires ms. The verb specifies GET, PUT, etc.
+		 * Notice that the URI is signed with the headers. If you use the URI,
+		 * ensure that you set those headers.
+		 *
+		 * @param expires validity period of the URI in milliseconds
+		 * @param verb the request HTTP verb
+		 */
 		URI signedUri(long expires, String verb) throws Exception;
 
 	}
 
 	public interface PutRequest extends CommonRequest<PutRequest> {
+		/**
+		 * Specify the encoding of the contents.
+		 * @param s encoding
+		 */
 		PutRequest contentEncoding(String s);
 
+		/**
+		 * ??
+		 */
 		PutRequest expect(String s);
 
+		/**
+		 * ??
+		 */
 		PutRequest expires(long ms);
 
+		/**
+		 * Specify the storage class
+		 */
 		PutRequest storageClass(StorageClass storageClass);
 
+		/**
+		 * Specify the content length ahead so no chunking necessary
+		 */
 		PutRequest contentLength(long length);
 
+		/**
+		 * Put the content
+		 * @param in the content
+		 */
 		void put(InputStream in) throws Exception;
 
 		/**
@@ -100,14 +256,5 @@ public interface Bucket {
 
 		ListRequest prefix(String prefix);
 	}
-
-	PutRequest putObject(String key);
-
-	GetRequest getObject(String key);
-
-	void delete(String key) throws Exception;
-
-	ListRequest listObjects() throws Exception;
-
 
 }
--- biz.aQute.api/src/main/java/biz/aQute/aws/ses/api/SES.java
+++ biz.aQute.api/src/main/java/biz/aQute/aws/ses/api/SES.java
@@ -1,29 +1,100 @@
 package biz.aQute.aws.ses.api;
 
+import org.osgi.annotation.versioning.ProviderType;
+
+/**
+ * An interface to the Simple Email Service from AWS.
+ * <p>
+ * This interface allows you to send emails.
+ */
+@ProviderType
 public interface SES {
 
+	/**
+	 * Create a new email.
+	 *
+	 * @param subject
+	 *            the subject of the mail
+	 * @return the email request builder
+	 */
 	SESRequest subject(String subject) throws Exception;
 
-
+	/**
+	 * The SES Request builder.
+	 *
+	 */
 	interface SESRequest {
 
+		/**
+		 * Provide HTML content
+		 *
+		 * @param html
+		 *            the html content
+		 */
 		SESRequest html(String html);
 
+		/**
+		 * Provide plain text content
+		 *
+		 * @param html
+		 *            the html content
+		 */
 		SESRequest text(String text);
 
+		/**
+		 * The destination
+		 *
+		 * @param html
+		 *            the html content
+		 */
 		SESRequest to(String address);
 
+		/**
+		 * The source
+		 *
+		 * @param address
+		 */
 
+		/**
+		 * The source
+		 *
+		 * @param address
+		 */
 		SESRequest from(String address);
 
+		/**
+		 * The blank carbon copy
+		 *
+		 * @param address
+		 */
 		SESRequest bcc(String address);
 
+		/**
+		 * Reply to
+		 *
+		 * @param address
+		 */
 		SESRequest replyTo(String address);
 
+		/**
+		 * Carbon Copy
+		 *
+		 * @param address
+		 */
 		SESRequest cc(String address);
 
+		/**
+		 * Return Path
+		 *
+		 * @param address
+		 */
 		SESRequest returnPath(String address);
 
+		/**
+		 * Send the email
+		 *
+		 * @return the email id
+		 */
 		String send() throws Exception;
 	}
 }
