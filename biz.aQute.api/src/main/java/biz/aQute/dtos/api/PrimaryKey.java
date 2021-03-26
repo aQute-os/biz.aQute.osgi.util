400da755d10d15dabee9a63125ead418329ac7b3
Peter Kriens <Peter.Kriens@aqute.biz>
2021-03-26 10:50:00 +0100

    primary  key retention & target

--- biz.aQute.api/src/main/java/biz/aQute/dtos/api/PrimaryKey.java
+++ biz.aQute.api/src/main/java/biz/aQute/dtos/api/PrimaryKey.java
@@ -1,6 +1,13 @@
 package biz.aQute.dtos.api;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * A primary key annotation
  */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
 public @interface PrimaryKey {}
