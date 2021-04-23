4a893e115b4a97a5d9fb142aec0a73526a4afe52
Peter Kriens <Peter.Kriens@aqute.biz>
2021-04-23 17:24:17 +0200

    [sshd] Buffer in wrapper
    Output was terribly slow, now buffer it.

--- biz.aQute.shell.sshd.provider/src/main/java/biz/aQute/shell/sshd/provider/OutputWrapper.java
+++ biz.aQute.shell.sshd.provider/src/main/java/biz/aQute/shell/sshd/provider/OutputWrapper.java
@@ -3,18 +3,18 @@ package biz.aQute.shell.sshd.provider;
 import java.io.IOException;
 import java.io.OutputStream;
 
-
 /**
- * Inserts a CR before a LF if it is missing
+ * Inserts a CR before a LF if it is missing. It also buffers the output until a flush is seen
  */
 public class OutputWrapper extends OutputStream {
 
 	private static final byte	LF		= '\n';
 	private static final byte	CR		= '\r';
-	private static final byte[]	CRLF	= { CR, LF };
 
 	final OutputStream			out;
 	byte						lastChar;
+	byte[]						buffer	= new byte[50000];
+	int							insert	= 0;
 
 	public OutputWrapper(OutputStream out) {
 		this.out = out;
@@ -22,38 +22,44 @@ public class OutputWrapper extends OutputStream {
 
 	@Override
 	public void write(int b) throws IOException {
-		synchronized (this) {
-			if (b == LF) {
-				if (lastChar != CR)
-					out.write(CRLF, 0, 2);
-				else
-					out.write(LF);
-			} else {
-				out.write(b);
-			}
-			lastChar = (byte) b;
+		synchronized (buffer) {
+			push(b);
 		}
 	}
 
 	public void write(byte buffer[], int off, int len) throws IOException {
-		synchronized (this) {
+		synchronized (buffer) {
 			for (int i = 0; i < len; i++) {
-				byte b = buffer[i];
-				if (b == LF) {
-					if (lastChar != CR)
-						out.write(CRLF, 0, 2);
-					else
-						out.write(LF);
-				} else {
-					out.write(b);
-				}
-				lastChar = (byte) b;
+				push(buffer[i + off]);
 			}
 		}
 	}
 
 	public void flush() throws IOException {
+		synchronized (buffer) {
+			flush0();
+		}
+	}
+
+	private void flush0() throws IOException {
+		out.write(buffer, 0, insert);
+		insert = 0;
 		out.flush();
+	}
+
+	private void push(int c) throws IOException {
+		if (  c == LF) {
+			if ( lastChar != CR)
+				store(CR);
+		}
+		store(c);
+	}
+
+	private void store(int c) throws IOException {
+		if (insert >= buffer.length) {
+			flush0();
+		}
+		lastChar = buffer[insert++] = (byte) (0xFF & c);
 	}
 
 }
