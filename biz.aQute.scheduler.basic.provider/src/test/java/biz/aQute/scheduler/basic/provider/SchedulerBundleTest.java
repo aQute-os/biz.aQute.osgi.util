9437c1d76311e4af617c3383cb7e8cc1dd73eed6
Peter Kriens <Peter.Kriens@aQute.biz>
2023-04-07 12:31:11 +0200

    The scheduler test hangs, more indications where

--- biz.aQute.scheduler.basic.provider/src/test/java/biz/aQute/scheduler/basic/provider/SchedulerBundleTest.java
+++ biz.aQute.scheduler.basic.provider/src/test/java/biz/aQute/scheduler/basic/provider/SchedulerBundleTest.java
@@ -19,10 +19,10 @@ import biz.aQute.scheduler.api.CronJob;
 public class SchedulerBundleTest {
 	static LaunchpadBuilder	builder	= new LaunchpadBuilder().bndrun("test.bndrun");
 
-	static Semaphore		present	= new Semaphore(0);
 
 	@Component(property = "cron=@reboot")
 	public static class TestRebootService implements CronJob {
+		static Semaphore		present	= new Semaphore(0);
 
 		@Override
 		public void run() throws Exception {
@@ -33,6 +33,7 @@ public class SchedulerBundleTest {
 
 	@Component(property = "cron=0/1 * * * * *")
 	public static class TestCleanup implements CronJob {
+		static Semaphore		present	= new Semaphore(0);
 
 		@Override
 		public void run() throws Exception {
@@ -44,12 +45,12 @@ public class SchedulerBundleTest {
 	@Test
 	public void testAnnotation() throws Exception {
 		try (Launchpad lp = builder.create()) {
-			present.drainPermits();
+			TestRebootService.present.drainPermits();
 			lp.bundle().addResource(TestRebootService.class).start();
 			Optional<CronJob> service = lp.getService(CronJob.class);
 			assertThat(service).isPresent();
 
-			boolean found = present.tryAcquire(1, 5000, TimeUnit.MILLISECONDS);
+			boolean found = TestRebootService.present.tryAcquire(1, 5000, TimeUnit.MILLISECONDS);
 			assertTrue(found);
 		}
 	}
@@ -57,24 +58,30 @@ public class SchedulerBundleTest {
 	@Test
 	public void testCleanedup() throws Exception {
 		try (Launchpad lp = builder.create().inject(this)) {
-			present.drainPermits();
+			System.out.println("clean up permits");
+			TestCleanup.present.drainPermits();
+			System.out.println("report the status");
 			lp.report();
 
+			System.out.println("create a component TestCleanup in a new bundle and runs once per second");
 			Bundle bundle = lp.bundle().addResource(TestCleanup.class).start();
 			Optional<CronJob> service = lp.getService(CronJob.class);
 			assertThat(service).isPresent();
 
-			boolean active = present.tryAcquire(2, 5000, TimeUnit.MILLISECONDS);
+			System.out.println("wait for the component to do its work");
+			boolean active = TestCleanup.present.tryAcquire(1, 5000, TimeUnit.MILLISECONDS);
 			assertTrue(active);
-
+			
+			System.out.println("stop the bundle");
 			bundle.stop();
+			TestCleanup.present.drainPermits();
 
+			System.out.println("make sure the CronJob is no longer there");
 			service = lp.getService(CronJob.class);
 			assertThat(service).isNotPresent();
 
-			Thread.sleep(1000);
-			present.drainPermits();
-			active = present.tryAcquire(1, 2000, TimeUnit.MILLISECONDS);
+			System.out.println("Wait for 3 seconds to see if the cron job is activated");
+			active = TestCleanup.present.tryAcquire(1, 3000, TimeUnit.MILLISECONDS);
 			assertFalse(active);
 
 		}
